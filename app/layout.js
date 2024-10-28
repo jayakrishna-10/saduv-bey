e8ebeeb345b8bf0af1b27e8e09ac5e8eb47d8d42
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-10-28 19:49:27 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?display=swap&family=Lexend:wght@400;500;700;900&family=Noto+Sans:wght@400;500;700;900"
+        />
       </head>
-      <body className="antialiased" style={{ fontFamily: 'Inter, sans-serif' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
