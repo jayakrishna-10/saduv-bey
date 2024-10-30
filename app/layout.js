6f3e82af7146294d4eb15bbe0befb8b9489c943e
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-10-31 00:23:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import './globals.css'
 import 'katex/dist/katex.min.css'
 
+// app/layout.js
 export const metadata = {
   title: {
     template: '%s | Saduv Bey',
@@ -13,18 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600;700&display=swap"
-        />
-      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
