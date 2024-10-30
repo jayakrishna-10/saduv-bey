8ade921b1173707c8f3785d3db021866e33d48b9
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-10-30 11:48:53 +0530

    Update NavBar.js

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -20,7 +20,8 @@ const NavBar = () => {
           </div>
 
           {/* Right side - Navigation */}
-          <div
+          <div>
+            
               href="/"
               className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition-colors"
             >
