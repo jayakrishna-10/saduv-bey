bbd1f149910be39efe58f179217b65626d215416
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-10-30 23:13:22 +0530

    Update card.js

--- app/components/ui/card.js
+++ app/components/ui/card.js
@@ -1,14 +1,10 @@
 // app/components/ui/card.js
 import * as React from "react";
-import { cn } from "@/lib/utils";
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border border-gray-200 bg-white text-gray-950 shadow-sm",
-      className
-    )}
+    className={`rounded-lg border border-gray-200 bg-white text-gray-950 shadow-sm ${className}`}
     {...props}
   />
 ));
