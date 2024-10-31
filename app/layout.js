3cc525caf0db93a3ebed26e2d4f4a2d4d119f6f9
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-10-31 08:45:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 import './globals.css'
 import 'katex/dist/katex.min.css'
 
-// app/layout.js
 export const metadata = {
   title: {
     template: '%s | Saduv Bey',
@@ -13,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full">
+      <body className="h-full">{children}</body>
     </html>
   );
 }
