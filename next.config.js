6f3321465a62ce67df9ab48d2dc2c6fe86805cc9
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2024-11-15 16:29:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    appDir: true,
-  }
+  
+  // Remove standalone output as it's not needed for Vercel
+  // Remove experimental.appDir as it's no longer needed in Next.js 14
 }
 
 module.exports = nextConfig
