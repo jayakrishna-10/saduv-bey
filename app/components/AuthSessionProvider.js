00c9d2d5308bd597bbda6d94ef94dc5f27fb483c
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2025-07-15 06:11:03 +0530

    Create AuthSessionProvider.js

---
+++ app/components/AuthSessionProvider.js
@@ -0,0 +1,10 @@
+// FILE: app/components/AuthSessionProvider.js
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+const AuthSessionProvider = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthSessionProvider;
